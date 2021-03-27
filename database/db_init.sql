c2f27bc00a422a1aee886ac99f40b5f656ff37b4
Artur Radiuk <226452@edu.p.lodz.pl>
2021-03-27 15:49:51 +0200

    entity_details table has been removed, initial data has been updated

--- database/db_init.sql
+++ database/db_init.sql
@@ -13,7 +13,7 @@ insert into alter_types(id, alter_type_name)
 values (-1, 'UPDATE');
 
 insert into alter_types(id, alter_type_name)
-values (-2, 'CREATE');
+values (-2, 'INSERT');
 
 insert into alter_types(id, alter_type_name)
 values (-3, 'DELETE');
@@ -21,76 +21,43 @@ values (-3, 'DELETE');
 --
 --
 --
--- entity_details start
-insert into entity_details (id, version, creation_datetime, last_alter_datetime, alter_type_id)
-values (-1, 0, now(), now(), -1);
-
-insert into entity_details (id, version, creation_datetime, last_alter_datetime, alter_type_id)
-values (-2, 0, now(), now(), -1);
-
-insert into entity_details (id, version, creation_datetime, last_alter_datetime, alter_type_id)
-values (-3, 0, now(), now(), -1);
-
-insert into entity_details (id, version, creation_datetime, last_alter_datetime, alter_type_id)
-values (-4, 0, now(), now(), -1);
-
-insert into entity_details (id, version, creation_datetime, last_alter_datetime, alter_type_id)
-values (-5, 0, now(), now(), -1);
-
-insert into entity_details (id, version, creation_datetime, last_alter_datetime, alter_type_id)
-values (-6, 0, now(), now(), -1);
-
-insert into entity_details (id, version, creation_datetime, last_alter_datetime, alter_type_id)
-values (-7, 0, now(), now(), -1);
-
-insert into entity_details (id, version, creation_datetime, last_alter_datetime, alter_type_id)
-values (-8, 0, now(), now(), -1);
-
-insert into entity_details (id, version, creation_datetime, last_alter_datetime, alter_type_id)
-values (-9, 0, now(), now(), -1);
-
-insert into entity_details (id, version, creation_datetime, last_alter_datetime, alter_type_id)
-values (-10, 0, now(), now(), -1);
-
-insert into entity_details (id, version, creation_datetime, last_alter_datetime, alter_type_id)
-values (-11, 0, now(), now(), -1);
--- entity_details end
---
---
---
 -- accounts start ||| password is 12345678
 insert into accounts(id, first_name, second_name, login, email, password_hash, confirmed, active,
                      last_incorrect_authentication_datetime, last_incorrect_authentication_logical_address,
                      last_correct_authentication_datetime, last_correct_authentication_logical_address,
-                     entity_details_id, language_type_id)
+                     entity_details_id, language_type_id,
+                     version, creation_datetime, last_alter_datetime, alter_type_id, created_by_id, altered_by_id)
 values (-1, 'Richard', 'Branson', 'rbranson', 'rbranson@gmail.com',
         '2634c3097f98e36865f0c572009c4ffd73316bc8b88ccfe8d196af35f46e2394',
-        true, true, null, null, null, null, -1, -1);
+        true, true, null, null, null, null, -1, -1, 0, now(), now(), -1, -1, -1);
 
 insert into accounts(id, first_name, second_name, login, email, password_hash, confirmed, active,
                      last_incorrect_authentication_datetime, last_incorrect_authentication_logical_address,
                      last_correct_authentication_datetime, last_correct_authentication_logical_address,
-                     entity_details_id, language_type_id)
+                     entity_details_id, language_type_id,
+                     version, creation_datetime, last_alter_datetime, alter_type_id, created_by_id, altered_by_id)
 values (-2, 'Elon', 'Musk', 'emusk', 'emusk@gmail.com',
         '2634c3097f98e36865f0c572009c4ffd73316bc8b88ccfe8d196af35f46e2394',
-        true, true, null, null, null, null, -2, -1);
+        true, true, null, null, null, null, -2, -1, 0, now(), now(), -1, -2, -2);
 
 insert into accounts(id, first_name, second_name, login, email, password_hash, confirmed, active,
                      last_incorrect_authentication_datetime, last_incorrect_authentication_logical_address,
                      last_correct_authentication_datetime, last_correct_authentication_logical_address,
-                     entity_details_id, language_type_id)
+                     entity_details_id, language_type_id,
+                     version, creation_datetime, last_alter_datetime, alter_type_id, created_by_id, altered_by_id)
 values (-3, 'Jeff', 'Bezos', 'jbezos', 'jbezos@gmail.com',
         '2634c3097f98e36865f0c572009c4ffd73316bc8b88ccfe8d196af35f46e2394',
-        true, true, null, null, null, null, -3, -1);
+        true, true, null, null, null, null, -3, -1, 0, now(), now(), -1, -3, -3);
 
 
 insert into accounts(id, first_name, second_name, login, email, password_hash, confirmed, active,
                      last_incorrect_authentication_datetime, last_incorrect_authentication_logical_address,
                      last_correct_authentication_datetime, last_correct_authentication_logical_address,
-                     entity_details_id, language_type_id)
+                     entity_details_id, language_type_id,
+                     version, creation_datetime, last_alter_datetime, alter_type_id, created_by_id, altered_by_id)
 values (-4, 'Mark', 'Zuckerberg', 'mzuckerberg', 'mzuckerberg@gmail.com',
         '2634c3097f98e36865f0c572009c4ffd73316bc8b88ccfe8d196af35f46e2394',
-        true, true, null, null, null, null, -4, -1);
+        true, true, null, null, null, null, -4, -1, 0, now(), now(), -1, -4, -4);
 -- accounts end
 --
 --
@@ -115,40 +82,46 @@ values (-5, 'moderator', -4, false);
 --
 --
 -- administrators start
-insert into administrators (id, entity_details_id)
-values (-1, -5);
+insert into administrators (id, version, creation_datetime, last_alter_datetime, alter_type_id, created_by_id,
+                            altered_by_id)
+values (-1, 0, now(), now(), -1, -1, -1);
 -- administrators end
 --
 --
 --
 -- business_workers start
-insert into business_workers (id, entity_details_id, phone_number)
-values (-4, -6, '0987654321');
+insert into business_workers (id, phone_number, version, creation_datetime, last_alter_datetime, alter_type_id,
+                              created_by_id, altered_by_id)
+values (-4, '0987654321', 0, now(), now(), -1, -3, -3);
 -- business_workers end
 --
 --
 --
 -- addresses start
-insert into addresses(id, house_number, street, post_code, city, country, entity_details_id)
-values (-1, 1, 'street 1', '123', 'London', 'United Kingdom', -7);
+insert into addresses(id, house_number, street, post_code, city, country,
+                      version, creation_datetime, last_alter_datetime, alter_type_id, created_by_id, altered_by_id)
+values (-1, 1, 'street 1', '123', 'London', 'United Kingdom', 0, now(), now(), -1, -2, -2);
 
-insert into addresses(id, house_number, street, post_code, city, country, entity_details_id)
-values (-2, 1, 'street 2', '321', 'Manchester', 'United Kingdom', -8);
+insert into addresses(id, house_number, street, post_code, city, country,
+                      version, creation_datetime, last_alter_datetime, alter_type_id, created_by_id, altered_by_id)
+values (-2, 1, 'street 2', '321', 'Manchester', 'United Kingdom', 0, now(), now(), -1, -3, -3);
 -- addresses end
 --
 --
 --
 -- clients start
-insert into clients(id, entity_details_id, phone_number, home_address_id)
-values (-2, -9, '123456789', -1);
+insert into clients(id, phone_number, home_address_id,
+                    version, creation_datetime, last_alter_datetime, alter_type_id, created_by_id, altered_by_id)
+values (-2, '123456789', -1, 0, now(), now(), -1, -2, -2);
 
-insert into clients(id, entity_details_id, phone_number, home_address_id)
-values (-3, -10, '123456789', -2);
+insert into clients(id, phone_number, home_address_id, version, creation_datetime, last_alter_datetime, alter_type_id,
+                    created_by_id, altered_by_id)
+values (-3, '123456789', -2, 0, now(), now(), -1, -3, -3);
 -- clients end
 --
 --
 --
 -- moderators start
-insert into moderators(id, entity_details_id)
-values (-5, -11)
+insert into moderators(id, version, creation_datetime, last_alter_datetime, alter_type_id, created_by_id, altered_by_id)
+values (-5, 0, now(), now(), -1, -4, -4)
 -- moderators end
--- database/db_structure.sql
+++ database/db_structure.sql
@@ -1,45 +1,30 @@
 create table language_types
 (
-    id                  bigint   not null,
+    id                  bigint  not null,
     language_type_value varchar not null unique,
     CONSTRAINT language_versions_primary_key_constraint PRIMARY KEY (id)
 );
 
 create table alter_types
 (
-    id              bigint   not null,
+    id              bigint  not null,
     alter_type_name varchar not null,
     CONSTRAINT alter_type_primary_key_constraint PRIMARY KEY (id)
 );
 
-create table entity_details
-(
-    id                  bigint     not null,
-    version             bigint     not null,
-    creation_datetime   timestamp not null,
---     created_by_id       bigint     not null, -- FOREIGN KEY
-    last_alter_datetime timestamp not null,
---     altered_by_id       bigint     not null, -- FOREIGN KEY
-    alter_type_id       bigint     not null, -- FOREIGN KEY
-
-    CONSTRAINT entity_details_primary_key_constraint PRIMARY KEY (id),
-    CONSTRAINT entity_details_alter_type_id_fk_constraint FOREIGN KEY (alter_type_id) REFERENCES alter_types (id)
-
-);
-
 create table accounts
 (
-    id                                            bigint   not null,
+    id                                            bigint    not null,
 
     first_name                                    varchar,
     second_name                                   varchar,
 
-    login                                         varchar not null,
-    email                                         varchar not null,
-    password_hash                                 varchar not null, -- hash sha256
+    login                                         varchar   not null,
+    email                                         varchar   not null,
+    password_hash                                 varchar   not null, -- hash sha256
 
-    confirmed                                     boolean not null,
-    active                                        boolean not null,
+    confirmed                                     boolean   not null,
+    active                                        boolean   not null,
 
     last_incorrect_authentication_datetime        timestamp,
     last_incorrect_authentication_logical_address varchar,
@@ -48,19 +33,31 @@ create table accounts
     last_correct_authentication_datetime          timestamp,
     last_correct_authentication_logical_address   varchar,
 
-    entity_details_id                             bigint   not null, -- FOREIGN KEY
-    language_type_id                              bigint   not null, -- FOREIGN KEY
+    entity_details_id                             bigint    not null, -- FOREIGN KEY
+    language_type_id                              bigint    not null, -- FOREIGN KEY
+
+    version                                       bigint    not null,
+    creation_datetime                             timestamp not null,
+    last_alter_datetime                           timestamp not null,
+
+    created_by_id                                 bigint    not null, -- FOREIGN KEY
+    altered_by_id                                 bigint    not null, -- FOREIGN KEY
+
+    alter_type_id                                 bigint    not null, -- FOREIGN KEY
 
     CONSTRAINT accounts_primary_key_constraint PRIMARY KEY (id),
-    CONSTRAINT accounts_entity_details_id_fk_constraint FOREIGN KEY (entity_details_id) REFERENCES entity_details (id),
-    CONSTRAINT accounts_language_type_id_fk_constraint FOREIGN KEY (language_type_id) REFERENCES language_types (id)
+    CONSTRAINT accounts_language_type_id_fk_constraint FOREIGN KEY (language_type_id) REFERENCES language_types (id),
+
+    CONSTRAINT accounts_created_by_id_fk_constraint FOREIGN KEY (created_by_id) REFERENCES accounts (id),
+    CONSTRAINT accounts_altered_by_id_fk_constraint FOREIGN KEY (created_by_id) REFERENCES accounts (id),
+    CONSTRAINT accounts_alter_type_id_fk_constraint FOREIGN KEY (alter_type_id) REFERENCES alter_types (id)
 );
 
 create table access_levels
 (
-    id           bigint   not null,
+    id           bigint  not null,
     access_level varchar not null,
-    account_id   bigint   not null,
+    account_id   bigint  not null,
     active       boolean not null,
 
     CONSTRAINT access_levels_id_primary_key_constraint PRIMARY KEY (id),
@@ -69,63 +66,107 @@ create table access_levels
 
 create table administrators
 (
-    id                bigint not null, -- should be foreign for access_level
-    entity_details_id bigint not null, -- FOREIGN KEY
+    id                  bigint    not null, -- should be foreign for access_level
+
+    version             bigint    not null,
+    creation_datetime   timestamp not null,
+    last_alter_datetime timestamp not null,
+    created_by_id       bigint    not null, -- FOREIGN KEY
+    altered_by_id       bigint    not null, -- FOREIGN KEY
+    alter_type_id       bigint    not null, -- FOREIGN KEY
 
     CONSTRAINT administrators_id_pk_constraint PRIMARY KEY (id),
     CONSTRAINT administrators_id_fk_constraint FOREIGN KEY (id) REFERENCES access_levels (id),
-    CONSTRAINT administrators_entity_details_id_fk_constraint FOREIGN KEY (entity_details_id) REFERENCES entity_details (id)
+
+    CONSTRAINT administrators_created_by_id_fk_constraint FOREIGN KEY (created_by_id) REFERENCES accounts (id),
+    CONSTRAINT administrators_altered_by_id_fk_constraint FOREIGN KEY (created_by_id) REFERENCES accounts (id),
+    CONSTRAINT administrators_alter_type_id_fk_constraint FOREIGN KEY (alter_type_id) REFERENCES alter_types (id)
 );
 
 create table business_workers
 (
-    id                bigint not null,
-    entity_details_id bigint not null, -- FOREIGN KEY
-    phone_number      varchar,
+    id                  bigint    not null,
+    phone_number        varchar,
+
+    version             bigint    not null,
+    creation_datetime   timestamp not null,
+    last_alter_datetime timestamp not null,
+    created_by_id       bigint    not null, -- FOREIGN KEY
+    altered_by_id       bigint    not null, -- FOREIGN KEY
+    alter_type_id       bigint    not null, -- FOREIGN KEY
 
     CONSTRAINT business_workers_id_pk_constraint PRIMARY KEY (id),
     CONSTRAINT business_workers_id_fk_constraint FOREIGN KEY (id) REFERENCES access_levels (id),
-    CONSTRAINT business_workers_entity_details_id_fk_constraint FOREIGN KEY (entity_details_id) REFERENCES entity_details (id)
+
+    CONSTRAINT business_workers_created_by_id_fk_constraint FOREIGN KEY (created_by_id) REFERENCES accounts (id),
+    CONSTRAINT business_workers_altered_by_id_fk_constraint FOREIGN KEY (created_by_id) REFERENCES accounts (id),
+    CONSTRAINT business_workers_alter_type_id_fk_constraint FOREIGN KEY (alter_type_id) REFERENCES alter_types (id)
 );
 
 create table addresses
 (
-    id                bigint   not null,
-    house_number      bigint   not null,
-    street            varchar not null,
-    post_code         varchar not null,
-    city              varchar not null,
-    country           varchar not null,
-    entity_details_id bigint   not null, -- FOREIGN KEY
+    id                  bigint    not null,
+    house_number        bigint    not null,
+    street              varchar   not null,
+    post_code           varchar   not null,
+    city                varchar   not null,
+    country             varchar   not null,
 
-    CONSTRAINT address_entity_details_id_fk_constraint FOREIGN KEY (entity_details_id) REFERENCES entity_details (id),
-    CONSTRAINT address_primary_key_constraint PRIMARY KEY (id)
+    version             bigint    not null,
+    creation_datetime   timestamp not null,
+    last_alter_datetime timestamp not null,
+    created_by_id       bigint    not null, -- FOREIGN KEY
+    altered_by_id       bigint    not null, -- FOREIGN KEY
+    alter_type_id       bigint    not null, -- FOREIGN KEY
+
+    CONSTRAINT address_primary_key_constraint PRIMARY KEY (id),
+
+    CONSTRAINT addresses_created_by_id_fk_constraint FOREIGN KEY (created_by_id) REFERENCES accounts (id),
+    CONSTRAINT addresses_altered_by_id_fk_constraint FOREIGN KEY (created_by_id) REFERENCES accounts (id),
+    CONSTRAINT addresses_alter_type_id_fk_constraint FOREIGN KEY (alter_type_id) REFERENCES alter_types (id)
 );
 
 create table clients
 (
-    id                bigint not null,
-    entity_details_id bigint not null, -- FOREIGN KEY
-    phone_number      varchar,
-    home_address_id   bigint,          -- FOREIGN KEY
+    id                  bigint    not null,
+    phone_number        varchar,
+    home_address_id     bigint,             -- FOREIGN KEY
+
+    version             bigint    not null,
+    creation_datetime   timestamp not null,
+    last_alter_datetime timestamp not null,
+    created_by_id       bigint    not null, -- FOREIGN KEY
+    altered_by_id       bigint    not null, -- FOREIGN KEY
+    alter_type_id       bigint    not null, -- FOREIGN KEY
 
     CONSTRAINT clients_id_pk_constraint PRIMARY KEY (id),
     CONSTRAINT clients_id_fk_constraint FOREIGN KEY (id) REFERENCES access_levels (id),
-    CONSTRAINT clients_entity_details_id_fk_constraint FOREIGN KEY (entity_details_id) REFERENCES entity_details (id),
-    CONSTRAINT clients_home_address_id_fk_constraint FOREIGN KEY (home_address_id) REFERENCES addresses (id)
+    CONSTRAINT clients_home_address_id_fk_constraint FOREIGN KEY (home_address_id) REFERENCES addresses (id),
+
+    CONSTRAINT clients_created_by_id_fk_constraint FOREIGN KEY (created_by_id) REFERENCES accounts (id),
+    CONSTRAINT clients_altered_by_id_fk_constraint FOREIGN KEY (created_by_id) REFERENCES accounts (id),
+    CONSTRAINT clients_alter_type_id_fk_constraint FOREIGN KEY (alter_type_id) REFERENCES alter_types (id)
 );
 
 create table moderators
 (
-    id                bigint not null,
-    entity_details_id bigint not null, -- FOREIGN KEY
+    id                  bigint    not null,
+
+    version             bigint    not null,
+    creation_datetime   timestamp not null,
+    last_alter_datetime timestamp not null,
+    created_by_id       bigint    not null, -- FOREIGN KEY
+    altered_by_id       bigint    not null, -- FOREIGN KEY
+    alter_type_id       bigint    not null, -- FOREIGN KEY
+
     CONSTRAINT moderators_id_pk_constraint PRIMARY KEY (id),
     CONSTRAINT moderators_id_fk_constraint FOREIGN KEY (id) REFERENCES access_levels (id),
-    CONSTRAINT moderators_entity_details_id_fk_constraint FOREIGN KEY (entity_details_id) REFERENCES entity_details (id)
+
+    CONSTRAINT moderators_created_by_id_fk_constraint FOREIGN KEY (created_by_id) REFERENCES accounts (id),
+    CONSTRAINT moderators_altered_by_id_fk_constraint FOREIGN KEY (created_by_id) REFERENCES accounts (id),
+    CONSTRAINT moderators_alter_type_id_fk_constraint FOREIGN KEY (alter_type_id) REFERENCES alter_types (id)
 );
 
-
--- drop table moderators,clients, addresses, business_workers, administrators, access_levels, accounts, entity_details,alter_types, language_types;
 
 
 CREATE VIEW glassfish_auth_view AS
@@ -136,20 +177,43 @@ WHERE accounts.confirmed
   AND accounts.active
   AND access_levels.active = true;
 
-select * from glassfish_auth_view;
+select *
+from glassfish_auth_view;
 
 -- Table owner --
-ALTER TABLE language_types OWNER TO ssbd03admin;
-ALTER TABLE alter_types OWNER TO ssbd03admin;
-ALTER TABLE entity_details OWNER TO ssbd03admin;
-ALTER TABLE accounts OWNER TO ssbd03admin;
-ALTER TABLE access_levels OWNER TO ssbd03admin;
-ALTER TABLE administrators OWNER TO ssbd03admin;
-ALTER TABLE business_workers OWNER TO ssbd03admin;
-ALTER TABLE addresses OWNER TO ssbd03admin;
-ALTER TABLE clients OWNER TO ssbd03admin;
-ALTER TABLE moderators OWNER TO ssbd03admin;
+ALTER TABLE language_types
+    OWNER TO ssbd03admin;
+ALTER TABLE alter_types
+    OWNER TO ssbd03admin;
+ALTER TABLE accounts
+    OWNER TO ssbd03admin;
+ALTER TABLE access_levels
+    OWNER TO ssbd03admin;
+ALTER TABLE administrators
+    OWNER TO ssbd03admin;
+ALTER TABLE business_workers
+    OWNER TO ssbd03admin;
+ALTER TABLE addresses
+    OWNER TO ssbd03admin;
+ALTER TABLE clients
+    OWNER TO ssbd03admin;
+ALTER TABLE moderators
+    OWNER TO ssbd03admin;
 ALTER VIEW glassfish_auth_view OWNER TO ssbd03admin;
 
 -- Table permissions --
 GRANT SELECT ON glassfish_auth_view TO ssbd03glassfish;
+
+
+-- Drops --
+drop view glassfish_auth_view;
+
+drop table moderators;
+drop table clients;
+drop table addresses;
+drop table business_workers;
+drop table administrators;
+drop table access_levels;
+drop table accounts;
+drop table alter_types;
+drop table language_types;
