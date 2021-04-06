9bbf9c028cdc7075d8661881b9e8596c1f826e56
216784 Jeziorski Szymon <216784@edu.p.lodz.pl>
2021-04-06 23:34:08 +0200

    Added JAVA EE constraints to entity members along with custom ones depending on created regexes.

--- src/main/java/pl/lodz/p/it/ssbd2021/ssbd03/mok/model/Address.java
+++ src/main/java/pl/lodz/p/it/ssbd2021/ssbd03/mok/model/Address.java
@@ -1,25 +1,36 @@
 package pl.lodz.p.it.ssbd2021.ssbd03.mok.model;
 
+import validators.City;
+import validators.Country;
+import validators.PostCode;
+import validators.Street;
+
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.Positive;
 
 @Entity(name = "addresses")
-public class Address  {
+public class Address {
     @Id
     private Long id;
 
+    @Positive
     @Column(name = "house_number")
     private Long houseNumber;
 
+    @Street
     private String street;
 
+    @PostCode
     @Column(name = "postal_code")
     private String postalCode;
 
+    @City
     private String city;
 
+    @Country
     private String country;
 
     @Embedded
--- src/main/java/pl/lodz/p/it/ssbd2021/ssbd03/mok/model/accesslevels/BusinessWorker.java
+++ src/main/java/pl/lodz/p/it/ssbd2021/ssbd03/mok/model/accesslevels/BusinessWorker.java
@@ -1,19 +1,23 @@
 package pl.lodz.p.it.ssbd2021.ssbd03.mok.model.accesslevels;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import pl.lodz.p.it.ssbd2021.ssbd03.mok.model.AccessLevel;
 import pl.lodz.p.it.ssbd2021.ssbd03.mok.model.AccessLevelType;
 import pl.lodz.p.it.ssbd2021.ssbd03.mok.model.EntityDetails;
+import validators.PhoneNumber;
 
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 
-@Data
 @Entity(name = "business_workers")
 @DiscriminatorValue("BusinessWorker")
 public class BusinessWorker extends AccessLevel {
+    @Getter
+    @Setter
+    @PhoneNumber
     @Column(name = "phone_number")
     private String phoneNumber;
 
