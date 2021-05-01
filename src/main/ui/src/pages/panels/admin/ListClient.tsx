ac39fc4864c0c95da5e05bf840b91a98b6328e83
224293 <224293@edu.p.lodz.pl>
2021-05-01 16:32:34 +0200

    Url has been corrected

--- src/main/ui/src/pages/panels/admin/ListClient.tsx
+++ src/main/ui/src/pages/panels/admin/ListClient.tsx
@@ -134,7 +134,7 @@ export default function AdminListClient() {
 
 
     const loadUsers = async () => {
-        const result = await axios.get('http://localhost:8080/cruisehub/api/account/accounts');
+        const result = await axios.get('http://localhost:8080/api/account/accounts');
         setUsers(result.data)
     }
 
