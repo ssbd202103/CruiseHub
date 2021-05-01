321a0705c5af177ab77ce08b402b616a7f2705a9
224293 <224293@edu.p.lodz.pl>
2021-05-01 16:59:20 +0200

    ETag has been added

--- src/main/java/pl/lodz/p/it/ssbd2021/ssbd03/mok/dto/AccountDtoForList.java
+++ src/main/java/pl/lodz/p/it/ssbd2021/ssbd03/mok/dto/AccountDtoForList.java
@@ -1,22 +1,22 @@
 package pl.lodz.p.it.ssbd2021.ssbd03.mok.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import pl.lodz.p.it.ssbd2021.ssbd03.entities.mok.AccessLevelType;
+import pl.lodz.p.it.ssbd2021.ssbd03.security.EntityIdentitySignerVerifier;
+import pl.lodz.p.it.ssbd2021.ssbd03.security.SignableEntity;
 import pl.lodz.p.it.ssbd2021.ssbd03.validators.Login;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
 import java.util.Set;
 
-
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-public class AccountDtoForList {
+public class AccountDtoForList implements SignableEntity {
 
     @Login
     private String login;
@@ -28,6 +28,27 @@ public class AccountDtoForList {
     @NotNull
     private boolean active;
 
+    @PositiveOrZero
+    private Long version;
+
     @NotNull
     private Set<AccessLevelType> accessLevels;
+
+    private String ETag;
+
+    @Override
+    public String getSignablePayload() {
+        return login + "." + version;
+    }
+
+    public AccountDtoForList(@Login String login, @Email String email, @NotNull boolean active, @PositiveOrZero Long version, @NotNull Set<AccessLevelType> accessLevels) {
+        this.login = login;
+        this.email = email;
+        this.active = active;
+        this.version = version;
+        this.accessLevels = accessLevels;
+        this.ETag = EntityIdentitySignerVerifier.calculateEntitySignature(this);
+    }
+
+
 }
