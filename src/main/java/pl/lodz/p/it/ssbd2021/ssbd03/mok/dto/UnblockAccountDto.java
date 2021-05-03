4868f7b18f2a04b76f10766f9a779392974a4ef9
224293 <224293@edu.p.lodz.pl>
2021-05-03 16:17:29 +0200

    UnblockAccountDto class has been added

---
+++ src/main/java/pl/lodz/p/it/ssbd2021/ssbd03/mok/dto/UnblockAccountDto.java
@@ -0,0 +1,24 @@
+package pl.lodz.p.it.ssbd2021.ssbd03.mok.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import pl.lodz.p.it.ssbd2021.ssbd03.security.SignableEntity;
+import pl.lodz.p.it.ssbd2021.ssbd03.validators.Login;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UnblockAccountDto implements SignableEntity {
+    @Login
+    private String login;
+
+    private Long version;
+
+    @JsonIgnore
+    @Override
+    public String getSignablePayload() {
+        return login + "." + version;
+    }
+}
