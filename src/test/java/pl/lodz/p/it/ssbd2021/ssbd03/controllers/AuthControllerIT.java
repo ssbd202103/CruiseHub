24e507bdb0bd00924738027b8d078a4dbc7a23cb
216784 Jeziorski Szymon <216784@edu.p.lodz.pl>
2021-05-20 14:41:10 +0200

    corrected hardcoded API url in AuthControllerIT

--- src/test/java/pl/lodz/p/it/ssbd2021/ssbd03/controllers/AuthControllerIT.java
+++ src/test/java/pl/lodz/p/it/ssbd2021/ssbd03/controllers/AuthControllerIT.java
@@ -3,13 +3,21 @@ package pl.lodz.p.it.ssbd2021.ssbd03.controllers;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import pl.lodz.p.it.ssbd2021.ssbd03.mok.dto.AuthenticateDto;
+import pl.lodz.p.it.ssbd2021.ssbd03.utils.PropertiesReader;
+
+import java.util.Properties;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class AuthControllerIT {
-    private static final String baseUri = "http://localhost:8080/api/";
+    private final String authBaseUri;
+
+    AuthControllerIT() {
+        Properties securityProperties = PropertiesReader.getSecurityProperties();
+        authBaseUri = securityProperties.getProperty("app.baseurl") + "/api/signin";
+    }
 
     @Test
     void auth() {
@@ -18,13 +26,13 @@ class AuthControllerIT {
 
         // correct test
         AuthenticateDto authInfo = new AuthenticateDto(correctLogin, correctPassword);
-        Response response = given().baseUri(baseUri).contentType("application/json").body(authInfo).post("signin/auth");
+        Response response = given().baseUri(authBaseUri).contentType("application/json").body(authInfo).post("/auth");
         assertFalse(response.getBody().asPrettyString().isEmpty());
         assertEquals(200, response.getStatusCode());
 
         // a test with an incorrect password
         AuthenticateDto authInfoFalse = new AuthenticateDto(correctLogin, "Incorre2*ctPassword");
-        Response responseFalse = given().baseUri(baseUri).contentType("application/json").body(authInfoFalse).post("signin/auth");
+        Response responseFalse = given().baseUri(authBaseUri).contentType("application/json").body(authInfoFalse).post("/auth");
         assertEquals(401, responseFalse.getStatusCode());
     }
 }
