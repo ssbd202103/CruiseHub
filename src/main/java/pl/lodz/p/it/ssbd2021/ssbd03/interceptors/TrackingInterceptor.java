dcf74548e0d9f699181abbe66a9df30f72bfcaf3
216784 Jeziorski Szymon <216784@edu.p.lodz.pl>
2021-05-23 00:32:59 +0200

    TrackingInterceptor implemented

---
+++ src/main/java/pl/lodz/p/it/ssbd2021/ssbd03/interceptors/TrackingInterceptor.java
@@ -0,0 +1,44 @@
+package pl.lodz.p.it.ssbd2021.ssbd03.interceptors;
+
+//import lombok.extern.slf4j.Slf4j;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+
+import javax.annotation.Resource;
+import javax.ejb.SessionContext;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.InvocationContext;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+
+//@Slf4j
+public class TrackingInterceptor {
+
+    private static final Logger logger = Logger.getLogger(TrackingInterceptor.class.getName());
+
+    @Resource
+    private SessionContext context;
+
+    @AroundInvoke
+    public Object logActions(InvocationContext context) throws Exception {
+
+        String resultString;
+        List<Object> paramList = context.getParameters() == null ? new ArrayList<>() : Arrays.asList(context.getParameters());
+        try {
+            Object result = context.proceed();
+            resultString = result == null ? "without return value" : "with returned value" + result;
+            logger.info(getLogString(context.getMethod().toString(), paramList, resultString));
+            return result;
+        } catch (Exception e) {
+            resultString = String.format("with exception thrown: \"%s\", cause: \"%s\", message: \"%s\"", e, e.getCause(), e.getMessage());
+            logger.severe(getLogString(context.getMethod().toString(), paramList, resultString));
+            throw e;
+        }
+    }
+
+    private String getLogString(String methodName, Iterable<Object> parameters, String resultString) {
+        return String.format("Method \"%s\" called by user \"%s\" with arguments \"%s\", \"%s\"", methodName, context.getCallerPrincipal().getName(), parameters, resultString);
+    }
+}
