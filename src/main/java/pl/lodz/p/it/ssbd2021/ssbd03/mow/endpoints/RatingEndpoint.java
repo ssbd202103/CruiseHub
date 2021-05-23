6ccfdc74d528814c8c049539a93c0ebd82ec8b68
226452 Artur Radiuk <226452@edu.p.lodz.pl>
2021-05-23 16:52:20 +0300

    TransactionAttribute attribute added to the RatingEndpoint and RatingManager - added; method declarations needed for the getting rating (mow.26) - declared (with RolesAllowed annotations);

--- src/main/java/pl/lodz/p/it/ssbd2021/ssbd03/mow/endpoints/RatingEndpoint.java
+++ src/main/java/pl/lodz/p/it/ssbd2021/ssbd03/mow/endpoints/RatingEndpoint.java
@@ -5,9 +5,16 @@ import pl.lodz.p.it.ssbd2021.ssbd03.mow.dto.RatingDto;
 import pl.lodz.p.it.ssbd2021.ssbd03.mow.dto.RemoveRankingDto;
 import pl.lodz.p.it.ssbd2021.ssbd03.mow.managers.RatingManagerLocal;
 
+import javax.annotation.security.RolesAllowed;
 import javax.ejb.EJB;
+import javax.ejb.TransactionAttribute;
 import javax.inject.Inject;
 
+import javax.ejb.TransactionAttributeType;
+
+import static javax.ejb.TransactionAttributeType.REQUIRES_NEW;
+
+@TransactionAttribute(REQUIRES_NEW)
 public class RatingEndpoint implements RatingEndpointLocal {
 
     @Inject
@@ -21,5 +28,11 @@ public class RatingEndpoint implements RatingEndpointLocal {
     @Override
     public void removeRating(RemoveRankingDto removeRankingDto) throws BaseAppException {
         ratingManager.removeRating(removeRankingDto.getLogin(), removeRankingDto.getCruiseGroupName());
+    }
+
+    @RolesAllowed("ownFindRating")
+    @Override
+    public RatingDto getRating(String login, String cruiseGroupName) throws BaseAppException {
+        return null; // todo finish implementation
     }
 }
