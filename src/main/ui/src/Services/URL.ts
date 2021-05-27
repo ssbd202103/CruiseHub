2d16f89bf8cbed5abe82b583e4273a7024a2300b
226452 <226452@edu.p.lodz.pl>
2021-05-27 13:39:25 +0300

    baseUrl updated

--- src/main/ui/src/Services/URL.ts
+++ src/main/ui/src/Services/URL.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    // baseURL: "http://studapp.it.p.lodz.pl:8003/api/"
-    baseURL: "https://localhost:8181/api/"
+    baseURL: "http://studapp.it.p.lodz.pl:8003/api/"
+    // baseURL: "https://localhost:8181/api/"
 });
