afbd115254d9252c81b04a26d020f8784dc4b6bf
Vitalii Shulhan <226454@edu.p.lodz.pl>
2021-05-30 18:10:33 +0200

    Implemented PanelLayout

--- src/main/ui/src/pages/panels/adminPanel.tsx
+++ src/main/ui/src/pages/panels/adminPanel.tsx
@@ -2,33 +2,20 @@ import {useTranslation} from 'react-i18next'
 import React, {useState} from "react";
 import ListClient from "./admin/ListClient";
 
-
-import {Grid, List, ListItem, ListItemIcon, ListItemText} from "@material-ui/core";
-
 import SettingsIcon from '@material-ui/icons/SettingsRounded'
 import AccountsListIcon from '@material-ui/icons/PeopleAltRounded'
-import GoBackIcon from '@material-ui/icons/ArrowBackRounded'
-
-import {Link, Redirect, Route, Switch} from 'react-router-dom'
-
-import PanelMenu from '../../components/PanelMenu'
 
 import ChangeAccountData from "./admin/ChangeAccountData"
 import GrantAccessLevel from "./admin/GrantAccessLevel"
 import ChangeAccessLevelState from "./admin/ChangeAccessLevelState"
 
-import styles from '../../styles/moderatorPanel.module.css'
-import AppColorSetter from "../../components/AppColorSetter";
-
 import {selectDarkMode} from "../../redux/slices/userSlice";
 import {useSelector} from "react-redux";
-import LogOutRoundedButton from "../../components/LogOutRoundedButton";
 import ChangeEmail from "../../components/changeData/ChangeEmail";
 import ChangePassword from "../../components/changeData/ChangePassword";
 import ChangeAdministratorData from "../../components/changeData/ChangeAdministratorData";
-import Header from "../../components/Header";
 import RequestSomeonePasswordReset from "../reset/requestSomeonesPasswordReset";
-import Breadcrumb from "../../components/Breadcrumb";
+import PanelLayout from "../../layouts/PanelLayout";
 
 export default function AdminPanel() {
     const {t} = useTranslation()
@@ -38,7 +25,6 @@ export default function AdminPanel() {
         setManage(true)
     }
 
-    const darkMode = useSelector(selectDarkMode)
 
     const [manageAccount, setManage] = useState(true)
     const handleManageAccount = () => {
@@ -69,71 +55,61 @@ export default function AdminPanel() {
     }
 
     return (
-        <>
-            <Header />
-            <Breadcrumb />
-            <Grid container className={styles.wrapper}>
-                <Grid item xs={2} md={3} xl={2}>
-                    <PanelMenu color={!darkMode ? 'yellow-dark' : 'white-light'}>
-                        <List className={styles.menu + ' ' + styles['menu-' + (!darkMode ? 'light' : 'dark')]} component="nav" aria-label="panel menu">
-                            <Link to="/accounts">
-                                <ListItem button>
-                                    <ListItemIcon>
-                                        <AccountsListIcon style={{ fill: `var(--${!darkMode ? 'white' : 'dark'})` }} />
-                                    </ListItemIcon>
-                                    <ListItemText> {t("list accounts")} </ListItemText>
-                                </ListItem>
-                            </Link>
-                            <Link to="/settings">
-                                <ListItem button>
-                                    <ListItemIcon>
-                                        <SettingsIcon style={{ fill: `var(--${!darkMode ? 'white' : 'dark'})` }} />
-                                    </ListItemIcon>
-                                    <ListItemText>{t("settings")}</ListItemText>
-                                </ListItem>
-                            </Link>
-
-                        </List>
-                    </PanelMenu>
-                </Grid>
-
-                <Grid item className={styles.content + ' ' + styles[`content-${!darkMode ? 'light' : 'dark'}`]} xs={10} md={9} xl={10}>
-                    <Route exact path="/accounts">
-                        <h3> {t("list accounts")} </h3>
-                        <ListClient />
-                    </Route>
-                    <Route path="/settings">
-                        <ChangeAdministratorData
-                            open={isDataEdit}
-                            onOpen={handleIsDataEdit}
-                            onConfirm={() => {setIsDataEdit(false)}}
-                            onCancel={() => {setIsDataEdit(false)}} />
-                        <ChangeEmail
-                            open={isEmailEdit}
-                            onOpen={handleIsEmailEdit}
-                            onConfirm={() => {setIsEmailEdit(false)}}
-                            onCancel={() => {setIsEmailEdit(false)}} />
-                        <ChangePassword
-                            open={isPasswordEdit}
-                            onOpen={handleIsPasswordEdit}
-                            onConfirm={() => {setIsPasswordEdit(false)}}
-                            onCancel={() => {setIsPasswordEdit(false)}} />
-                    </Route>
-
-                    <Route path="/accounts/change_account_data">
-                        <ChangeAccountData/>
-                    </Route>
-                    <Route path="/accounts/grant_access_level">
-                        <GrantAccessLevel/>
-                    </Route>
-                    <Route path="/accounts/change_access_level_state">
-                        <ChangeAccessLevelState/>
-                    </Route>
-                    <Route path="/accounts/resetSomebodyPassword">
-                        <RequestSomeonePasswordReset/>
-                    </Route>
-                </Grid>
-            </Grid>
-        </>
+        <PanelLayout
+            color={{
+                light: 'yellow-dark',
+                dark: 'white-light'
+            }}
+            menu={[
+                {
+                    link: '/accounts',
+                    Icon: AccountsListIcon,
+                    text: t('list accounts'),
+                    Component: ListClient
+                },
+                {
+                    link: '/settings',
+                    Icon: SettingsIcon,
+                    text: t('settings'),
+                    Component: () => (
+                        <>
+                            <ChangeAdministratorData
+                                open={isDataEdit}
+                                onOpen={handleIsDataEdit}
+                                onConfirm={() => {setIsDataEdit(false)}}
+                                onCancel={() => {setIsDataEdit(false)}} />
+                            <ChangeEmail
+                                open={isEmailEdit}
+                                onOpen={handleIsEmailEdit}
+                                onConfirm={() => {setIsEmailEdit(false)}}
+                                onCancel={() => {setIsEmailEdit(false)}} />
+                            <ChangePassword
+                                open={isPasswordEdit}
+                                onOpen={handleIsPasswordEdit}
+                                onConfirm={() => {setIsPasswordEdit(false)}}
+                                onCancel={() => {setIsPasswordEdit(false)}} />
+                        </>
+                    )
+                }
+            ]}
+            otherRoutes={[
+                {
+                    to: '/accounts/change_account_data',
+                    Component: ChangeAccountData
+                },
+                {
+                    to: '/accounts/grant_access_level',
+                    Component: GrantAccessLevel
+                },
+                {
+                    to: '/accounts/change_access_level_state',
+                    Component: ChangeAccessLevelState
+                },
+                {
+                    to: '/accounts/resetSomebodyPassword',
+                    Component: RequestSomeonePasswordReset
+                }
+            ]}
+        />
     )
 }
--- src/main/ui/src/pages/panels/moderatorPanel.tsx
+++ src/main/ui/src/pages/panels/moderatorPanel.tsx
@@ -4,25 +4,14 @@ import ListClient from "./moderator/ListClient";
 
 import SettingsIcon from '@material-ui/icons/SettingsRounded'
 import AccountsListIcon from '@material-ui/icons/PeopleAltRounded'
-import GoBackIcon from '@material-ui/icons/ArrowBackRounded'
 
-import {Grid, List, ListItem, ListItemIcon, ListItemText} from "@material-ui/core";
-
-import {Link, Redirect, Route} from 'react-router-dom'
-
-import PanelMenu from '../../components/PanelMenu'
-import styles from '../../styles/moderatorPanel.module.css'
 import {useSelector} from "react-redux";
 import {selectDarkMode} from "../../redux/slices/userSlice";
-import AppColorSetter from "../../components/AppColorSetter";
-import LogOutRoundedButton from "../../components/LogOutRoundedButton";
 import ChangeEmail from "../../components/changeData/ChangeEmail";
 import ChangePassword from "../../components/changeData/ChangePassword";
 import ChangeModeratorData from "../../components/changeData/ChangeModeratorData";
 import ManageWorkers from "./moderator/ManageBusinessWorkers";
-import ChangeAccountData from "./admin/ChangeAccountData";
-import Header from "../../components/Header";
-import Breadcrumb from "../../components/Breadcrumb";
+import PanelLayout from "../../layouts/PanelLayout";
 
 
 export default function ModeratorPanel() {
@@ -64,68 +53,52 @@ export default function ModeratorPanel() {
     }
 
     return (
-        <>
-            <Header />
-            <Breadcrumb />
-            <Grid container className={styles.wrapper}>
-                <Grid item xs={2} md={3} xl={2}>
-                    <PanelMenu color={!darkModel ? 'pink-dark' : 'white'}>
-                        <List className={styles.menu + ' ' + styles['menu-' + (!darkModel ? 'light' : 'dark')]} component="nav" aria-label="panel menu">
-                            <Link to="/accounts">
-                                <ListItem button>
-                                    <ListItemIcon>
-                                        <AccountsListIcon style={{ fill: `var(--${!darkModel ? 'white' : 'dark'})` }} />
-                                    </ListItemIcon>
-                                    <ListItemText> {t("list accounts")} </ListItemText>
-                                </ListItem>
-                            </Link>
-                            <Link to="/ManageWorkers">
-                                <ListItem button>
-                                    <ListItemIcon>
-                                        <AccountsListIcon style={{ fill: `var(--${!darkModel ? 'white' : 'dark'})` }} />
-                                    </ListItemIcon>
-                                    <ListItemText> {t("Manage business workers")} </ListItemText>
-                                </ListItem>
-                            </Link>
-                            <Link to="/settings">
-                                <ListItem button>
-                                    <ListItemIcon>
-                                        <SettingsIcon style={{ fill: `var(--${!darkModel ? 'white' : 'dark'})` }} />
-                                    </ListItemIcon>
-                                    <ListItemText>{t("settings")}</ListItemText>
-                                </ListItem>
-                            </Link>
-                        </List>
-                    </PanelMenu>
-                </Grid>
-
-                <Grid item className={styles.content + ' ' + styles[`content-${!darkModel ? 'light' : 'dark'}`]} xs={10} md={9} xl={10}>
-                    <Route exact path="/accounts">
-                        <h3> {t("list accounts")} </h3>
-                        <ListClient />
-                    </Route>
-                    <Route exact path="/settings">
-                        <ChangeModeratorData
-                            open={isDataEdit}
-                            onOpen={handleIsDataEdit}
-                            onConfirm={() => {setIsDataEdit(false)}}
-                            onCancel={() => {setIsDataEdit(false)}} />
-                        <ChangeEmail
-                            open={isEmailEdit}
-                            onOpen={handleIsEmailEdit}
-                            onConfirm={() => {setIsEmailEdit(false)}}
-                            onCancel={() => {setIsEmailEdit(false)}} />
-                        <ChangePassword
-                            open={isPasswordEdit}
-                            onOpen={handleIsPasswordEdit}
-                            onConfirm={() => {setIsPasswordEdit(false)}}
-                            onCancel={() => {setIsPasswordEdit(false)}} />
-                    </Route>
-                    <Route exact path="/ManageWorkers">
-                        <ManageWorkers/>
-                    </Route>
-                </Grid>
-            </Grid>
-        </>
+        <PanelLayout
+            color={{
+                    light: 'pink-dark',
+                    dark: 'white'
+            }}
+            menu={[
+                {
+                    link: '/accounts',
+                    text: t('list accounts'),
+                    Icon: AccountsListIcon,
+                    Component: ListClient
+                },
+                {
+                    link: '/ManageWorkers',
+                    text: t('Manage business workers'),
+                    Icon: AccountsListIcon,
+                    Component: ManageWorkers
+                },
+                {
+                    link: '/settings',
+                    text: t('settings'),
+                    Icon: SettingsIcon,
+                    Component: () => (
+                        <>
+                            <ChangeModeratorData
+                                open={isDataEdit}
+                                onOpen={handleIsDataEdit}
+                                onConfirm={() => {setIsDataEdit(false)}}
+                                onCancel={() => {setIsDataEdit(false)}}
+                            />
+                            <ChangeEmail
+                                open={isEmailEdit}
+                                onOpen={handleIsEmailEdit}
+                                onConfirm={() => {setIsEmailEdit(false)}}
+                                onCancel={() => {setIsEmailEdit(false)}}
+                            />
+                            <ChangePassword
+                                open={isPasswordEdit}
+                                onOpen={handleIsPasswordEdit}
+                                onConfirm={() => {setIsPasswordEdit(false)}}
+                                onCancel={() => {setIsPasswordEdit(false)}}
+                            />
+                        </>
+                    )
+                }
+            ]}
+        />
     )
 }
