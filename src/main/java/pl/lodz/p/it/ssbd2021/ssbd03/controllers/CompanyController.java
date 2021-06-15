70f7eddb51bc47507ec5041e15f8b485e353177f
224325 Kempa Grzegorz <224325@edu.p.lodz.pl>
2021-06-16 01:16:58 +0200

    Update CompanyController.java
    fix imports

--- src/main/java/pl/lodz/p/it/ssbd2021/ssbd03/controllers/CompanyController.java
+++ src/main/java/pl/lodz/p/it/ssbd2021/ssbd03/controllers/CompanyController.java
@@ -2,11 +2,9 @@ package pl.lodz.p.it.ssbd2021.ssbd03.controllers;
 
 import pl.lodz.p.it.ssbd2021.ssbd03.exceptions.BaseAppException;
 import pl.lodz.p.it.ssbd2021.ssbd03.mok.dto.BusinessWorkerDto;
+import pl.lodz.p.it.ssbd2021.ssbd03.mow.dto.companies.AddCompanyDto;
 import pl.lodz.p.it.ssbd2021.ssbd03.mow.dto.companies.CompanyLightDto;
 import pl.lodz.p.it.ssbd2021.ssbd03.mow.dto.companies.CompanyDto;
-import pl.lodz.p.it.ssbd2021.ssbd03.mow.dto.AddCompanyDto;
-import pl.lodz.p.it.ssbd2021.ssbd03.mow.dto.CompanyLightDto;
-import pl.lodz.p.it.ssbd2021.ssbd03.mow.dto.changeCruiseGroup.CompanyDto;
 import pl.lodz.p.it.ssbd2021.ssbd03.mow.endpoints.CompanyEndpointLocal;
 
 import javax.enterprise.context.RequestScoped;
