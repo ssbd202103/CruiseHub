3053388a7aed359d1ac8958731896428856a0685
Szymon Wróbel <szym.wrobel@gmail.com>
2021-06-23 13:30:45 +0200

    Fixed the bug which allowed booking a started cruise

--- src/main/ui/src/i18n.ts
+++ src/main/ui/src/i18n.ts
@@ -227,6 +227,7 @@ const resources = {
             "companies_nip_unique_constraint": "Firma o podanym numerze NIP znajduje się już w bazie danych",
             "companies_name_unique_constraint": "Firma o podanej nazwie znajduje się już w bazie danych",
             "cruise.cannot.cancel.started": "Nie możesz anulować wycieczki, która się już rozpoczęła",
+            "cruise.cannot.book.started": "Nie możesz zarezerwować wycieczki, która się już rozpoczęła",
 
             //auth errors
             "auth.incorrect.login": "Błąd logowania",
@@ -645,6 +646,7 @@ const resources = {
             "companies_nip_unique_constraint": "Company with said NIP already exists in the database",
             "companies_name_unique_constraint": "Company with said name already exists in the database",
             "cruise.cannot.cancel.started": "You cannot cancel a cruise that has started",
+            "cruise.cannot.book.started": "You cannot book a cruise that has started",
 
             //auth errors
             "auth.incorrect.login": "Login error",
