e01082b915ee6b17e5c611dc41fe09edefcbaf4a
arturradiuk <arturradiuk@gmail.com>
2021-12-10 18:59:56 +0100

    temp

--- src/main/ui/src/Services/URL.ts
+++ src/main/ui/src/Services/URL.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: window.location.href.includes("studapp") ?
-        "https://studapp.it.p.lodz.pl:8403/api" : "http://localhost:8080/api/"
+        "https://studapp.it.p.lodz.pl:8403/api" : "https://localhost:8443/api/"
 });
