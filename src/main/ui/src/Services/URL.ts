6a2f69d306b4b9d54c47b666481a78a1a699e4c3
216784 Jeziorski Szymon <216784@edu.p.lodz.pl>
2021-12-11 13:39:33 +0100

    fix incorrect port being used for sending requests

--- src/main/ui/src/Services/URL.ts
+++ src/main/ui/src/Services/URL.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: window.location.href.includes("studapp") ?
-        "https://studapp.it.p.lodz.pl:8403/api" : "http://localhost:8080/api/"
+        "https://studapp.it.p.lodz.pl:8403/api" : "https://127.0.0.1:555/api/"
 });
