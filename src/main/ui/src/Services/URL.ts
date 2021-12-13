a592fd7ea87756df651c532df6035aff400a6632
arturradiuk <arturradiuk@gmail.com>
2021-12-13 12:08:04 +0100

    urls updated;

--- src/main/ui/src/Services/URL.ts
+++ src/main/ui/src/Services/URL.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: window.location.href.includes("studapp") ?
-        "https://studapp.it.p.lodz.pl:8403/api" : "https://127.0.0.1:555/api/"
+        "https://studapp.it.p.lodz.pl:8403/api" : "http://ssbd03-app-temp-tua02.apps.okd.cti.p.lodz.pl/api/"
 });
